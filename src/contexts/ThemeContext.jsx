5a1ee17418f067e8a7d05888b8a42ac380f2ab0e
Rafiur Rahman <rrfoundary@gmail.com>
2021-08-14 08:49:20 +0600

    refactor themecontext using hooks

--- src/contexts/ThemeContext.jsx
+++ src/contexts/ThemeContext.jsx
@@ -1,27 +1,18 @@
-import React, { createContext, Component } from "react";
+import React, { createContext, Component, useState } from "react";
 
 export const ThemeContext = createContext();
-
-class ThemeProvider extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      isDarkMode: true,
-    };
-    this.handleThemeChange = this.handleThemeChange.bind(this);
-  }
-  handleThemeChange() {
-    this.setState({ isDarkMode: !this.state.isDarkMode });
-  }
-  render() {
-    return (
-      <ThemeContext.Provider
-        value={{ ...this.state, onThemeChange: this.handleThemeChange }}
-      >
-        {this.props.children}
-      </ThemeContext.Provider>
-    );
-  }
-}
+const ThemeProvider = (props) => {
+  const [isDarkMode, setIsDarkMode] = useState(false);
+  const handleThemeChange = () => {
+    setIsDarkMode(!isDarkMode);
+  };
+  return (
+    <ThemeContext.Provider
+      value={{ isDarkMode, onThemeChange: handleThemeChange }}
+    >
+      {props.children}
+    </ThemeContext.Provider>
+  );
+};
 
 export default ThemeProvider;
