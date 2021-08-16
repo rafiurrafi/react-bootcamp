2800ead378cc2833de2b7affa2314f7b7fbe8174
Rafiur Rahman <rrfoundary@gmail.com>
2021-08-16 08:17:41 +0600

    added edit functionliaty

--- src/todos/todo.jsx
+++ src/todos/todo.jsx
@@ -1,5 +1,29 @@
 import React from "react";
 const Todo = (props) => {
+  const [isEditing, setIsEditing] = React.useState(false);
+  function handleUpdate(e) {
+    e.preventDefault();
+    setIsEditing();
+  }
+  let result;
+  if (!isEditing) {
+    result = (
+      <div>
+        <button>Edit</button>
+        <button onClick={props.onRemove}>X</button>
+        <li>{props.task}</li>
+      </div>
+    );
+  } else {
+    result = (
+      <div>
+        <form onSubmit={this.handleUpdate}>
+          <input type="text" />
+          <button>update</button>
+        </form>
+      </div>
+    );
+  }
   return (
     <div>
       <button>Edit</button>
