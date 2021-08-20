b466c0e1aae51a71d8d6d4dbee095c6a6de7d4a4
Rafiur Rahman <rrfoundary@gmail.com>
2021-08-20 09:54:33 +0600

    refactor using hook reusable

--- src/todos/hook-intro/toggler.jsx
+++ src/todos/hook-intro/toggler.jsx
@@ -1,17 +1,13 @@
-import React, { useState } from 'react';
+import React from 'react';
+import useToggle from './hooks/useToggle';
 const Toggler = (props) => {
-    const [isHappy, setIsHappy] = useState(false);
-    const [isHeartBroken, setIsHeartBroken] = useState(false);
-    const handleIsHappy = () => {
-        setIsHappy(!isHappy)
-    }
-    const handleIsHeartBroken = () => {
-        setIsHeartBroken(!isHeartBroken)
-    }
+    const [isHappy, setIsHappy] = useToggle(false);
+    const [isHeartBroken, setIsHeartBroken] = useToggle(false);
+    
     return ( 
         <div>
-            <h1 onClick = {handleIsHappy}>{isHappy ? "😊" : "😢"} </h1>
-            <h1 onClick = {handleIsHeartBroken}>{isHeartBroken ? "💖" : "💔"} </h1>
+            <h1 onClick = {setIsHappy}>{isHappy ? "😊" : "😢"} </h1>
+            <h1 onClick = {setIsHeartBroken}>{isHeartBroken ? "💖" : "💔"} </h1>
         </div>
      );
 }
