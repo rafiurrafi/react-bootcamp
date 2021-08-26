05d1252223d09adb85de701a8129af25fd94b191
Rafiur Rahman <rrfoundary@gmail.com>
2021-08-26 08:09:31 +0600

    remove previous works

--- src/todos/hook-intro/hooks/useInput.jsx
+++
@@ -1,9 +0,0 @@
-import {useState} from 'react';
-const useInput = ({initialValue}) => {
-    const [input, setInput] = useState(initialValue);
-    const toggle = (e) => {
-        setInput(e.target.value)
-    }
-    return [input, toggle]
-}
-export default useInput
--- src/todos/hook-intro/hooks/useToggle.jsx
+++
@@ -1,9 +0,0 @@
-import { useState } from "react";
-const useToggle = (initialValue = false) => {
-    const [state, setState] = useState(initialValue);
-    const toggle = () => {
-        setState(!state)
-    }
-    return [state, toggle]
-}
-export default useToggle;
