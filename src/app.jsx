b53eae77a49a354f10671934fe6ab5e22dfef9e4
Rafiur Rahman <rrfoundary@gmail.com>
2021-09-17 06:39:53 +0600

    cleaning everything for master project

--- src/app.jsx
+++ src/app.jsx
@@ -1,33 +1,6 @@
 import React from "react";
-import CardList from "./components/cardList";
-import SearchBox from "./searchBox";
-
-class App extends React.Component {
-  state = {
-    monsters: [],
-    searchValue: "",
-  };
-  componentDidMount() {
-    fetch("https://jsonplaceholder.typicode.com/users")
-      .then((response) => response.json())
-      .then((monsters) => this.setState({ monsters }));
-  }
-  render() {
-    const { monsters, searchValue } = this.state;
-    const filterMonster = monsters.filter((monster) =>
-      monster.name.toLowerCase().includes(searchValue.toLowerCase())
-    );
-    return (
-      <div>
-        <SearchBox
-          placeholder="Search monster"
-          value={this.state.searchValue}
-          handleChange={(e) => this.setState({ searchValue: e.target.value })}
-        />
-        <CardList monsters={filterMonster} />
-      </div>
-    );
-  }
-}
+const App = (props) => {
+  return <h1>Hello master project</h1>;
+};
 
 export default App;
