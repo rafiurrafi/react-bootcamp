c5dd70c2b967d498ff68f1c70e9d56451c3354ea
Rafiur Rahman <rrfoundary@gmail.com>
2021-09-18 06:08:23 +0600

    Fixed pallete route

--- src/app.jsx
+++ src/app.jsx
@@ -1,12 +1,27 @@
 import React from "react";
 import Palette from "./components/palette";
 import seedColors from "./seedColors";
+import { Route, Switch } from "react-router-dom";
 import { generatePalette } from "./components/utils/colorHelpers";
 const App = (props) => {
+  const findPalette = (id) => {
+    return seedColors.find((color) => color.id === id);
+  };
   return (
-    <div>
-      <Palette palette={generatePalette(seedColors[4])} />
-    </div>
+    <Switch>
+      <Route
+        path="/palette/:id"
+        render={(routeProps) => (
+          <Palette
+            palette={generatePalette(findPalette(routeProps.match.params.id))}
+          />
+        )}
+      />
+      <Route path="/" render={() => <h1>Home</h1>} />
+    </Switch>
+    // <div>
+    //   <Palette palette={generatePalette(seedColors[4])} />
+    // </div>
   );
 };
 
