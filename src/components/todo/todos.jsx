09c697a825b3c294ad199070e78fd8db816d9102
Rafiur Rahman <rrfoundary@gmail.com>
2021-09-23 16:10:45 +0600

    refactor todos component using hook

--- src/components/todo/todos.jsx
+++ src/components/todo/todos.jsx
@@ -2,25 +2,23 @@ import { List, Paper } from "@material-ui/core";
 import React from "react";
 import TodoList from "./todoList";
 
-class Todos extends React.Component {
-  render() {
-    const { onToggleTodos, onDeleteTodos, onEditTodos } = this.props;
-    return (
-      <Paper>
-        <List>
-          {this.props.todos.map((todo) => (
-            <TodoList
-              key={todo.key}
-              todo={todo}
-              onEditTodos={onEditTodos}
-              onToggleTodos={onToggleTodos}
-              onDeleteTodos={onDeleteTodos}
-            />
-          ))}
-        </List>{" "}
-      </Paper>
-    );
-  }
-}
+const Todos = (props) => {
+  const { todos, onToggleTodos, onDeleteTodos, onEditTodos } = props;
+  return (
+    <Paper>
+      <List>
+        {todos.map((todo) => (
+          <TodoList
+            key={todo.key}
+            todo={todo}
+            onEditTodos={onEditTodos}
+            onToggleTodos={onToggleTodos}
+            onDeleteTodos={onDeleteTodos}
+          />
+        ))}
+      </List>{" "}
+    </Paper>
+  );
+};
 
 export default Todos;
