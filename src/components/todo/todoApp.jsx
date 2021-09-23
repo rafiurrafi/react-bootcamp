0d8536084fc23094e05d6fd44445d0b1cb1db2c4
Rafiur Rahman <rrfoundary@gmail.com>
2021-09-23 08:15:27 +0600

    refactor todoapp

--- src/components/todo/todoApp.jsx
+++ src/components/todo/todoApp.jsx
@@ -1,5 +1,5 @@
 import { Grid } from "@material-ui/core";
-import React from "react";
+import React, { useState } from "react";
 import Navbar from "./navbar";
 import TodoForm from "./todoForm";
 import { withStyles } from "@material-ui/styles";
@@ -11,73 +11,51 @@ const styles = {
     margin: 30,
   },
 };
-
-class TodoApp extends React.Component {
-  static defaultProps = {
-    initialTodos: [
-      { _id: uuid(), task: "Todo", completed: false },
-      { _id: uuid(), task: "New", completed: false },
-    ],
-  };
-  constructor(props) {
-    super(props);
-    this.state = {
-      todos: [],
-    };
-    this.addTask = this.addTask.bind(this);
-    this.editTodos = this.editTodos.bind(this);
-    this.toggleTodos = this.toggleTodos.bind(this);
-    this.deleteTodos = this.deleteTodos.bind(this);
+const TodoApp = (props) => {
+  const initialTodos = [
+    { _id: uuid(), task: "Todo", completed: false },
+    { _id: uuid(), task: "New", completed: false },
+  ];
+  const [todos, setTodos] = useState(initialTodos);
+  const { classes } = props;
+  function addTask(task) {
+    setTodos([...todos, { _id: uuid(), task, completed: false }]);
   }
-  componentDidMount() {
-    this.setState({ todos: [...this.props.initialTodos] });
-  }
-  addTask(task) {
-    this.setState({
-      todos: [...this.state.todos, { _id: uuid(), task, completed: false }],
-    });
-  }
-  toggleTodos(id) {
-    console.log(id);
-    const updatedTodos = this.state.todos.map((todo) =>
+  function toggleTodos(id) {
+    const updatedTodos = todos.map((todo) =>
       todo._id === id ? { ...todo, completed: !todo.completed } : todo
     );
-
-    this.setState({ todos: updatedTodos });
+    setTodos(updatedTodos);
   }
-  editTodos(id, task) {
-    const todos = this.state.todos.map((todo) =>
+  function editTodos(id, task) {
+    const todos = todos.map((todo) =>
       todo._id === id ? { ...todo, task } : todo
     );
-    this.setState({ todos });
+    setTodos(todos);
   }
-  deleteTodos(id) {
-    console.log(id);
-    const updatedTodos = this.state.todos.filter((todo) => todo._id !== id);
-    this.setState({ todos: updatedTodos });
+  function deleteTodos(id) {
+    const updatedTodos = todos.filter((todo) => todo._id !== id);
+    setTodos(updatedTodos);
   }
-  render() {
-    const { classes } = this.props;
-    return (
-      <div>
-        <Navbar />
-        <div className={classes.spacing} />
-        <Grid container justifyContent="center" spacing={2}>
-          <Grid item xs={10} md={6} lg={4}>
-            <TodoForm onAddTask={this.addTask} />
-          </Grid>
-          <Grid item xs={10} md={6} lg={4}>
-            <Todos
-              todos={this.state.todos}
-              onEditTodos={this.editTodos}
-              onToggleTodos={this.toggleTodos}
-              onDeleteTodos={this.deleteTodos}
-            />
-          </Grid>
+  return (
+    <div>
+      <Navbar />
+      <div className={classes.spacing} />
+      <Grid container justifyContent="center" spacing={2}>
+        <Grid item xs={10} md={6} lg={4}>
+          <TodoForm onAddTask={addTask} />
         </Grid>
-      </div>
-    );
-  }
-}
+        <Grid item xs={10} md={6} lg={4}>
+          <Todos
+            todos={todos}
+            onEditTodos={editTodos}
+            onToggleTodos={toggleTodos}
+            onDeleteTodos={deleteTodos}
+          />
+        </Grid>
+      </Grid>
+    </div>
+  );
+};
 
 export default withStyles(styles)(TodoApp);
