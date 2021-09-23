cfa2a8cb533e80541a025ab231fe7da110c0f674
Rafiur Rahman <rrfoundary@gmail.com>
2021-09-23 08:04:09 +0600

    refactor edit form

--- src/components/todo/editForm.jsx
+++ src/components/todo/editForm.jsx
@@ -1,39 +1,54 @@
 import { TextField } from "@material-ui/core";
-import React from "react";
-
-class EditForm extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      task: "",
-    };
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
+import React, { useEffect, useState } from "react";
+const EditForm = (props) => {
+  const [task, setTask] = useState("");
+  useEffect(() => {
+    setTask(props.todo.task);
+    console.log(task);
+  }, []);
+  function handleChange(e) {
+    setTask(e.target.value);
   }
-  componentDidMount() {
-    this.setState({ task: this.props.todo.task });
-  }
-  handleChange(e) {
-    this.setState({ task: e.target.value });
-  }
-  handleSubmit(e) {
+  function handleSubmit(e) {
     e.preventDefault();
-    this.props.onEditTodos(this.props.todo._id, this.state.task);
-    this.props.onToggleEdit();
+    props.onEditTodos(props.todo._id, task);
+    props.onToggleEdit();
   }
-  render() {
-    return (
-      <div>
-        <form onSubmit={this.handleSubmit}>
-          <TextField
-            fullWidth
-            value={this.state.task}
-            onChange={this.handleChange}
-          />
-        </form>
-      </div>
-    );
-  }
-}
+  return (
+    <form onSubmit={handleSubmit}>
+      <TextField fullWidth value={task} onChange={handleChange} />
+    </form>
+  );
+};
 
 export default EditForm;
+// class EditForm extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       task: "",
+//     };
+//     this.handleChange = this.handleChange.bind(this);
+//     this.handleSubmit = this.handleSubmit.bind(this);
+//   }
+//   componentDidMount() {
+//     this.setState({ task: this.props.todo.task });
+//   }
+//   handleChange(e) {
+//     this.setState({ task: e.target.value });
+//   }
+//   handleSubmit(e) {
+//     e.preventDefault();
+//     this.props.onEditTodos(this.props.todo._id, this.state.task);
+//     this.props.onToggleEdit();
+//   }
+//   render() {
+//     return (
+//       <div>
+
+//       </div>
+//     );
+//   }
+// }
+
+// export default EditForm;
--- src/components/todo/navbar.jsx
+++ src/components/todo/navbar.jsx
@@ -1,18 +1,14 @@
 import React from "react";
 import { AppBar, Toolbar, Typography } from "@material-ui/core";
 
-class Navbar extends React.Component {
-  render() {
-    return (
-      <div>
-        <AppBar position="static">
-          <Toolbar>
-            <Typography variant="h5">TodoList</Typography>
-          </Toolbar>
-        </AppBar>
-      </div>
-    );
-  }
-}
+const Navbar = (props) => {
+  return (
+    <AppBar position="static">
+      <Toolbar>
+        <Typography variant="h5">TodoList</Typography>
+      </Toolbar>
+    </AppBar>
+  );
+};
 
 export default Navbar;
