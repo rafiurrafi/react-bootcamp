c815775bf93fd78aafd3737210facf6e8f795ff9
Rafiur Rahman <rrfoundary@gmail.com>
2021-10-05 14:59:28 +0600

    crwn - added header component and routing to home page

--- src/app.jsx
+++ src/app.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Route, Switch } from "react-router";
 import "./app.css";
+import Header from "./components/header/header";
 import HomePage from "./pages/homepage/homepage";
 import ShopPage from "./pages/shop/shopPage";
 
@@ -8,6 +9,7 @@ class App extends React.Component {
   render() {
     return (
       <div>
+        <Header />
         <Switch>
           <Route path="/shop" component={ShopPage} />
           <Route path="/" component={HomePage} />
