2fe3f572e2c75480e47bee3d87f33334011a6448
Rafiur Rahman <rrfoundary@gmail.com>
2021-10-24 15:58:35 +0600

    added use call back to prevent ref rendering

--- src/practise/practiseApp.jsx
+++ src/practise/practiseApp.jsx
@@ -1,20 +1,24 @@
-import React, { useState } from "react";
+import React, { useCallback, useState } from "react";
 import Button from "./components/button";
 import ShowCount from "./components/showCount";
 import Title from "./components/title";
 const PractiseApp = (props) => {
   const [count1, setCount1] = useState(0);
   const [count2, setCount2] = useState(0);
+  const handleClick1 = useCallback(() => {
+    setCount1((count) => count + 1);
+  }, []);
+  const handleClick2 = useCallback(() => {
+    setCount2((count) => count + 5);
+  }, []);
   return (
     <div>
       <Title />
       <ShowCount count={count1} title="Counter 1" />
-      <Button handleClick={() => setCount1(count1 + 1)}>
-        Increment By One
-      </Button>
+      <Button handleClick={handleClick1}>Increment By One</Button>
       <hr />
       <ShowCount count={count2} title="Counter 1" />
-      <Button handleClick={() => setCount2(count2 + 5)} title="Counter 5">
+      <Button handleClick={handleClick2} title="Counter 5">
         Increment By five
       </Button>
     </div>
