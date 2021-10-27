a43f04c760f0fdc20dd6a82bb0a9857dbc79d521
Rafiur Rahman <rrfoundary@gmail.com>
2021-10-27 09:59:50 +0600

    added home post post single page

---
+++ src/practise/components/singlePost.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SignlePost = () => {
+  return <h1>SignlePost</h1>;
+};
+
+export default SignlePost;
--- src/practise/practiseApp.jsx
+++ src/practise/practiseApp.jsx
@@ -1,10 +1,16 @@
+import { Switch } from "@material-ui/core";
 import React from "react";
-import Button from "./components/button";
+import { Route } from "react-router";
+import Home from "./components/home";
+import Post from "./components/post";
+import SignlePost from "./components/singlePost";
 const PractiseApp = (props) => {
   return (
-    <div>
-      <Button />
-    </div>
+    <Switch>
+      <Route path="/post/:id" component={SignlePost} />
+      <Route path="/post" component={Post} />
+      <Route path="/" component={Home} />
+    </Switch>
   );
 };
 
