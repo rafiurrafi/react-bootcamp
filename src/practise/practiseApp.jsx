f89df5778e04e9d4556d6c23cceef0a7be855a84
Rafiur Rahman <rrfoundary@gmail.com>
2021-11-05 20:31:31 +0600

    review the use memo callback

--- src/practise/practiseApp.jsx
+++ src/practise/practiseApp.jsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 import Button from "./components/button";
 import ShowCounter from "./components/showCounter";
 import Title from "./components/title";
+
 const PractiseApp = (props) => {
   const [count1, setCount1] = useState(0);
   const [count2, setCount2] = useState(0);
@@ -10,11 +11,6 @@ const PractiseApp = (props) => {
     () => setCount1((count) => count + 1),
     []
   );
-  const isEven = React.useMemo(() => {
-    var i = 0;
-    while (i < 1000000000) i++;
-    return count1 % 2 ? "Odd" : "Even";
-  }, [count1]);
   const handleIncrementByFive = React.useCallback(
     () => setCount2((count) => count + 5),
     []
@@ -24,8 +20,7 @@ const PractiseApp = (props) => {
       <Title title="Welcome to our app" />
       <ShowCounter number={1} count={count1} />
       <Button onHandleIncrement={handleIncrementByOne}>Increment By One</Button>
-      <br />
-      {isEven}
+
       <hr />
       <ShowCounter number={2} count={count2} />
       <Button onHandleIncrement={handleIncrementByFive}>
