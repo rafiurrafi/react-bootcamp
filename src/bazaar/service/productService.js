01d271cc0ef00be1e6b9f6ff538c4997895ff8cc
Rafiur Rahman <rrfoundary@gmail.com>
2021-11-09 08:57:25 +0600

    bazaar - added image as resources

---
+++ src/bazaar/service/productService.js
@@ -0,0 +1,84 @@
+import uuid from "uuid/v4";
+const products = [
+  {
+    id: uuid(),
+    name: "apples",
+    img: "/img/fruites/Apples.jpg",
+    stock: 10,
+    price: 399.99,
+    shortDesc: "Nulla facilisi. Curabitur at lacus ac velit ornare lobortis.",
+    description:
+      "Cras sagittis. Praesent nec nisl a purus blandit viverra. Ut leo. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Fusce a quam.",
+  },
+  {
+    id: uuid(),
+    name: "orange",
+    img: "/img/fruites/BabySpinach.jpg",
+    stock: 10,
+    price: 399.99,
+    shortDesc: "Nulla facilisi. Curabitur at lacus ac velit ornare lobortis.",
+    description:
+      "Cras sagittis. Praesent nec nisl a purus blandit viverra. Ut leo. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Fusce a quam.",
+  },
+  {
+    id: uuid(),
+    name: "blackberries",
+    img: "/img/fruites/beef.jpg",
+    stock: 10,
+    price: 399.99,
+    shortDesc: "Nulla facilisi. Curabitur at lacus ac velit ornare lobortis.",
+    description:
+      "Cras sagittis. Praesent nec nisl a purus blandit viverra. Ut leo. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Fusce a quam.",
+  },
+  {
+    id: uuid(),
+    name: "jackfruits",
+    img: "/img/fruites/BrusselsSprouts.jpg",
+    stock: 10,
+    price: 399.99,
+    shortDesc: "Nulla facilisi. Curabitur at lacus ac velit ornare lobortis.",
+    description:
+      "Cras sagittis. Praesent nec nisl a purus blandit viverra. Ut leo. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Fusce a quam.",
+  },
+  {
+    id: uuid(),
+    name: "pineapples",
+    img: "/img/fruites/CelerySticks.jpg",
+    stock: 10,
+    price: 399.99,
+    shortDesc: "Nulla facilisi. Curabitur at lacus ac velit ornare lobortis.",
+    description:
+      "Cras sagittis. Praesent nec nisl a purus blandit viverra. Ut leo. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Fusce a quam.",
+  },
+  {
+    id: uuid(),
+    name: "Baby Spinach",
+    img: "/img/fruites/CelerySticks.jpg",
+    stock: 10,
+    price: 399.99,
+    shortDesc: "Nulla facilisi. Curabitur at lacus ac velit ornare lobortis.",
+    description:
+      "Cras sagittis. Praesent nec nisl a purus blandit viverra. Ut leo. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Fusce a quam.",
+  },
+  {
+    id: uuid(),
+    name: "Celery Stick",
+    img: "/img/fruites/chicken_brest_ribcxo.jpg",
+    stock: 10,
+    price: 399.99,
+    shortDesc: "Nulla facilisi. Curabitur at lacus ac velit ornare lobortis.",
+    description:
+      "Cras sagittis. Praesent nec nisl a purus blandit viverra. Ut leo. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Fusce a quam.",
+  },
+  {
+    id: uuid(),
+    name: "Clementines",
+    img: "/img/fruites/chicken_thigh.jpg",
+    stock: 10,
+    price: 399.99,
+    shortDesc: "Nulla facilisi. Curabitur at lacus ac velit ornare lobortis.",
+    description:
+      "Cras sagittis. Praesent nec nisl a purus blandit viverra. Ut leo. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Fusce a quam.",
+  },
+];
+export default products;
