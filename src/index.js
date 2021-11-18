a1da14677789ac9c2c9eec3e00d4aa906565cea0
Rafiur Rahman <rrfoundary@gmail.com>
2021-11-18 18:42:11 +0600

    intenso - introduce new home page for testing

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import ReactDOM from "react-dom";
 // import App from "./app";
 import { BrowserRouter } from "react-router-dom";
-import BazaarApp from "./bazaar/bazaarApp";
+// import BazaarApp from "./bazaar/bazaarApp";
+import IntensoApp from "./intenso/intensoApp";
 // import PractiseApp from "./practise/practiseApp";
 // import AmazonApp from "./amazon/amazonApp";
 ReactDOM.render(
@@ -9,7 +10,8 @@ ReactDOM.render(
     {/* <App /> */}
     {/* <AmazonApp /> */}
     {/* <PractiseApp /> */}
-    <BazaarApp />
+    {/* <BazaarApp /> */}
+    <IntensoApp />
   </BrowserRouter>,
   // <BazaarApp />,
   document.querySelector("#root")
---
+++ src/intenso/intensoApp.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+const IntensoApp = (props) => {
+  return <h1>IntensoApp</h1>;
+};
+
+export default IntensoApp;
