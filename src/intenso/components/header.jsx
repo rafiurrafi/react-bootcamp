25fe449b90a39f73717c07c1a64207ce0ddb66d9
Rafiur Rahman <rrfoundary@gmail.com>
2021-11-18 23:56:59 +0600

    added navbar for mobile menu

--- src/intenso/components/header.jsx
+++ src/intenso/components/header.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import img from "../assets/logo.png";
 import Button from "./common/button";
@@ -6,6 +6,7 @@ import Navbar from "./navbar";
 
 import "../styles/header.scss";
 const Header = () => {
+  const [openMenu, setOpenMenu] = useState(false);
   return (
     <header className="header">
       <Link>
@@ -15,6 +16,34 @@ const Header = () => {
       <div className="header__buttons">
         <Button label="Log in" full={true} size="sm" />
         <Button label="Create account" size="sm" />
+      </div>
+      <div className="header__mobile-menu">
+        <div
+          className={`header__mobile-menu-icon ${openMenu && "change"}`}
+          onClick={() => setOpenMenu(!openMenu)}
+        >
+          <div class="bar1"></div>
+          <div class="bar2"></div>
+          <div class="bar3"></div>
+        </div>
+        <div className={`mobile-menu__content ${openMenu && "change"}`}>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/work">How's Work</Link>
+              </li>
+              <li>
+                <Link to="/track">Track My Order</Link>
+              </li>
+              <li>
+                <Link to="/about">About Us</Link>
+              </li>
+            </ul>
+          </nav>
+        </div>
       </div>
     </header>
   );
