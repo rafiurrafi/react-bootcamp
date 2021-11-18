0d356b9c6055048f31c009ef487817d910062524
Rafiur Rahman <rrfoundary@gmail.com>
2021-11-19 00:22:06 +0600

    intenso - mobile menu working fine

--- src/intenso/components/header.jsx
+++ src/intenso/components/header.jsx
@@ -1,12 +1,12 @@
-import React, { useState } from "react";
+import React from "react";
 import { Link } from "react-router-dom";
 import img from "../assets/logo.png";
 import Button from "./common/button";
 import Navbar from "./navbar";
 
 import "../styles/header.scss";
+import MobileMenu from "./mobileMenu";
 const Header = () => {
-  const [openMenu, setOpenMenu] = useState(false);
   return (
     <header className="header">
       <Link>
@@ -17,34 +17,7 @@ const Header = () => {
         <Button label="Log in" full={true} size="sm" />
         <Button label="Create account" size="sm" />
       </div>
-      <div className="header__mobile-menu">
-        <div
-          className={`header__mobile-menu-icon ${openMenu && "change"}`}
-          onClick={() => setOpenMenu(!openMenu)}
-        >
-          <div class="bar1"></div>
-          <div class="bar2"></div>
-          <div class="bar3"></div>
-        </div>
-        <div className={`mobile-menu__content ${openMenu && "change"}`}>
-          <nav>
-            <ul>
-              <li>
-                <Link to="/">Home</Link>
-              </li>
-              <li>
-                <Link to="/work">How's Work</Link>
-              </li>
-              <li>
-                <Link to="/track">Track My Order</Link>
-              </li>
-              <li>
-                <Link to="/about">About Us</Link>
-              </li>
-            </ul>
-          </nav>
-        </div>
-      </div>
+      <MobileMenu />
     </header>
   );
 };
