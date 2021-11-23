0c94140e1aec651711f85211661a81ca83c3537d
Rafiur Rahman <rrfoundary@gmail.com>
2021-11-23 12:30:53 +0600

    bazar - fix banner slider margin right

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import ReactDOM from "react-dom";
 // import App from "./app";
 import { BrowserRouter } from "react-router-dom";
-// import BazaarApp from "./bazaar/bazaarApp";
-import IntensoApp from "./intenso/intensoApp";
+import BazaarApp from "./bazaar/bazaarApp";
+// import IntensoApp from "./intenso/intensoApp";
 // import PractiseApp from "./practise/practiseApp";
 // import AmazonApp from "./amazon/amazonApp";
 ReactDOM.render(
@@ -10,8 +10,8 @@ ReactDOM.render(
     {/* <App /> */}
     {/* <AmazonApp /> */}
     {/* <PractiseApp /> */}
-    {/* <BazaarApp /> */}
-    <IntensoApp />
+    <BazaarApp />
+    {/* <IntensoApp /> */}
   </BrowserRouter>,
   // <BazaarApp />,
   document.querySelector("#root")
