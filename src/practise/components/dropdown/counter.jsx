eb4b7f5f2e17ff7045c3a2e57f73eef4a8a50257
Rafiur Rahman <rrfoundary@gmail.com>
2021-11-26 04:09:11 +0600

    implement basic functionlity of useReducer

--- src/practise/components/dropdown/counter.jsx
+++ src/practise/components/dropdown/counter.jsx
@@ -1,7 +1,18 @@
-import React from "react";
-
+import React, { useReducer } from "react";
+function nameReducer(state, newState) {
+  return newState;
+}
 const Counter = (props) => {
-  return <div className="counter"></div>;
+  const [name, setName] = useReducer(nameReducer, "");
+  const handleChange = (e) => {
+    setName(e.target.value);
+  };
+  return (
+    <div className="counter">
+      <input value={name} onChange={handleChange} />
+      <p>{name}</p>
+    </div>
+  );
 };
 
 export default Counter;
