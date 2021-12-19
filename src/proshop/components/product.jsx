2185797d2a1264b5e41f58839209233f0ebf7648
Rafiur Rahman <rrfoundary@gmail.com>
2021-12-19 22:41:18 +0600

    proshop - added the rating components

--- src/proshop/components/product.jsx
+++ src/proshop/components/product.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Card } from "react-bootstrap";
+import Rating from "./rating";
 
 const Product = ({ product }) => {
   return (
@@ -7,6 +8,20 @@ const Product = ({ product }) => {
       <a href={`/product/${product._id}`}>
         <Card.Img src={product.image} varinat="top" />
       </a>
+      <Card.Body>
+        <a href={`/product/${product._id}`}>
+          <Card.Title as="div">
+            <strong>{product.name}</strong>
+          </Card.Title>
+        </a>
+        <Card.Text as="div" className="my-3">
+          <Rating
+            value={product.rating}
+            text={`${product.numReviews} reviews`}
+          />
+        </Card.Text>
+        <Card.Text as="h3">${product.price}</Card.Text>
+      </Card.Body>
     </Card>
   );
 };
