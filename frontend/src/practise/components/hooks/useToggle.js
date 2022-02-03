97404d4634824761517b4706862f2e4eaa1289b4
Rafiur Rahman <rrfoundary@gmail.com>
2022-02-03 12:36:20 +0600

    practise - removed unnecessary markup

--- frontend/src/practise/components/hooks/useToggle.js
+++
@@ -1,9 +0,0 @@
-import { useState } from "react";
-const useToggle = (initialValue = false) => {
-  const [state, setState] = useState(initialValue);
-  const handleState = () => {
-    setState(!state);
-  };
-  return [state, handleState];
-};
-export default useToggle;
