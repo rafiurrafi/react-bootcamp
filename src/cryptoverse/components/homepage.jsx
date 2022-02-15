61c130d6be25fbb747f1bf37daae69ea038862ac
Rafiur Rahman <rrfoundary@gmail.com>
2022-02-15 10:43:31 +0600

    cryptoverse - added navbar component with link

--- src/cryptoverse/components/homepage.jsx
+++ src/cryptoverse/components/homepage.jsx
@@ -1,7 +1,35 @@
 import React from "react";
-
+import { Typography, Row, Col, Statistic } from "antd";
+import millify from "millify";
+import { useGetCryptosQuery } from "../services/cryptoApi";
 const HomePage = () => {
-  return <h1>Homepage</h1>;
+  const { Title } = Typography;
+  const [data, isFetching] = useGetCryptosQuery();
+  console.log(data);
+  return (
+    <>
+      <Title level={2} className="heading">
+        Statistics
+      </Title>
+      <Row>
+        <Col span={12}>
+          <Statistic title="Total cryptocurrencies" value={5} />
+        </Col>
+        <Col span={12}>
+          <Statistic title="Total exchange" value={5} />
+        </Col>
+        <Col span={12}>
+          <Statistic title="Total market cap" value={5} />
+        </Col>
+        <Col span={12}>
+          <Statistic title="Total 24h volume" value={5} />
+        </Col>
+        <Col span={12}>
+          <Statistic title="Total market" value={5} />
+        </Col>
+      </Row>
+    </>
+  );
 };
 
 export default HomePage;
--- src/cryptoverse/cryptoverseApp.jsx
+++ src/cryptoverse/cryptoverseApp.jsx
@@ -9,27 +9,31 @@ import CryptoDetails from "./components/cryptoDetails";
 import Exchange from "./components/exchange";
 import News from "./components/news";
 import "antd/dist/antd.css";
+import { Provider } from "react-redux";
+import store from "./app/store";
 const CryptoverseApp = () => {
   return (
-    <div>
-      <div className="navbar">
-        <Navbar />
+    <Provider value={store}>
+      <div>
+        <div className="navbar">
+          <Navbar />
+        </div>
+        <div className="main">
+          <Layout>
+            <div className="routes">
+              <Routes>
+                <Route path="/crypto/:id" element={CryptoDetails} />
+                <Route path="/cryptocurrencies" element={Cryptocureencies} />
+                <Route path="/exchange" element={Exchange} />
+                <Route path="/news" element={News} />
+                <Route path="/" element={HomePage} />
+              </Routes>
+            </div>
+          </Layout>
+        </div>
+        <div className="footer"></div>
       </div>
-      <div className="main">
-        <Layout>
-          <div className="routes">
-            <Routes>
-              <Route path="/crypto/:id" element={CryptoDetails} />
-              <Route path="/cryptocurrencies" element={Cryptocureencies} />
-              <Route path="/exchange" element={Exchange} />
-              <Route path="/news" element={News} />
-              <Route path="/" element={HomePage} />
-            </Routes>
-          </div>
-        </Layout>
-      </div>
-      <div className="footer"></div>
-    </div>
+    </Provider>
   );
 };
 
