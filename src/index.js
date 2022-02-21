da77742c28c9fbe8e2109339f6cf5b0398695ebf
Rafiur Rahman <rrfoundary@gmail.com>
2022-02-21 07:53:12 +0600

    newzeela - introduce new project for news portal using react

--- src/index.js
+++ src/index.js
@@ -1,7 +1,4 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter } from "react-router-dom";
-import BullTokeApp from "./bullToke/bullTokeApp";
-// import CryptoverseApp from "./cryptoverse/cryptoverseApp";
-// import GeoHotelApp from "./geoHotel/geoHotelApp";
-ReactDOM.render(<BullTokeApp />, document.querySelector("#root"));
+import NewzeelaApp from "./newzeela/newzeelaApp";
+ReactDOM.render(<NewzeelaApp />, document.querySelector("#root"));
---
+++ src/newzeela/newzeelaApp.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+// https://bytesed.com/tf/katerio/index-04.html
+const NewzeelaApp = () => {
+  return (
+    <div className="newzeela-app">
+      <h1>Our news portal</h1>
+    </div>
+  );
+};
+
+export default NewzeelaApp;
