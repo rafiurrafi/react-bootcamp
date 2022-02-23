581bbc58fcc057eb3a3f04b8d2040c7ed7a7b567
Rafiur Rahman <rrfoundary@gmail.com>
2022-02-23 08:23:37 +0600

    newzeela - addd top header component

--- src/newzeela/newzeelaApp.jsx
+++ src/newzeela/newzeelaApp.jsx
@@ -1,9 +1,15 @@
 import React from "react";
+import HeaderTop from "./components/headerTop";
+import "./components/styles/newzeelaApp.scss";
+
+// react icons
+
+// link inspiration
 // https://bytesed.com/tf/katerio/index-04.html
 const NewzeelaApp = () => {
   return (
     <div className="newzeela-app">
-      <h1>Our news portal</h1>
+      <HeaderTop />
     </div>
   );
 };
