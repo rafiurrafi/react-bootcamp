6ccadce5c0df9c905727ac40761bbbf41427b449
Rafiur Rahman <rrfoundary@gmail.com>
2022-02-25 09:39:07 +0600

    newzeela - added header middle component

--- src/newzeela/components/headerMiddle.jsx
+++ src/newzeela/components/headerMiddle.jsx
@@ -1,5 +1,7 @@
 import React from "react";
+import Banner from "./common/banner";
 import "./styles/headerMiddle.scss";
+import bannerImg from "../assets/banner-1.jpg";
 const HeaderMiddle = () => {
   return (
     <div className="header-middle">
@@ -9,6 +11,9 @@ const HeaderMiddle = () => {
       <div className="header__search">
         <input type="text" placeholder="Search" />
         <i className="fa fa-search"></i>
+      </div>
+      <div className="header__banner">
+        <Banner imgSrc={bannerImg} />
       </div>
     </div>
   );
