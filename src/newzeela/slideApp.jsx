8600477483e982b36d13c414b545a04784ef8f40
Rafiur Rahman <rrfoundary@gmail.com>
2022-03-08 11:14:19 +0600

    newzeela - added react slick slider

--- src/newzeela/slideApp.jsx
+++ src/newzeela/slideApp.jsx
@@ -1,64 +1,57 @@
-import React, { useState } from "react";
-import "./slideApp.scss";
-const slides = [
+import React from "react";
+
+const imageData = [
   {
     _id: 1,
-    image:
-      "https://media.istockphoto.com/photos/heart-shaped-red-box-with-ribbon-on-a-pink-pastel-background-pattern-picture-id1300580149?b=1&k=20&m=1300580149&s=170667a&w=0&h=1y1nQu1WPx4dDTaLTAM73ZN-Heo4DeuslDMmQbTA4GY=",
+    imgSrc:
+      "https://media.istockphoto.com/photos/giza-at-sunset-picture-id1281134453?b=1&k=20&m=1281134453&s=170667a&w=0&h=goc9eGY4AtHPoYStQ7Y681AdLiErlnlGXDLnz5Wu3dA=",
   },
   {
     _id: 2,
-    image:
-      "https://media.istockphoto.com/photos/valentines-day-composition-with-rose-flower-red-heart-and-gift-boxes-picture-id1361735480?b=1&k=20&m=1361735480&s=170667a&w=0&h=SpJf67QqFQbF5UzRh2X3mFb1U28Q-SpDO3w7GVenJ54=",
+    imgSrc:
+      "https://images.unsplash.com/photo-1539768942893-daf53e448371?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8ZWd5cHR8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60",
   },
   {
     _id: 3,
-    image:
-      "https://media.istockphoto.com/photos/white-rose-flower-isolated-on-white-picture-id1328152519?b=1&k=20&m=1328152519&s=170667a&w=0&h=s2MwATe-EbogQWM5lu1a82Qi9iPdzJKGEoSDMC7LwaM=",
+    imgSrc:
+      "https://images.unsplash.com/photo-1502250493741-939d1c76eaad?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OXx8ZWd5cHR8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60",
   },
   {
     _id: 4,
-    image:
-      "https://media.istockphoto.com/photos/red-rose-picture-id1298194099?b=1&k=20&m=1298194099&s=170667a&w=0&h=L1WYQNJ__EFnuxJf5FyeltRncVCpV8vvuaqvlVvKwtA=",
+    imgSrc:
+      "https://images.unsplash.com/photo-1632944398987-494eebe663be?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTl8fGVneXB0fGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
   },
   {
     _id: 5,
-    image:
-      "https://images.unsplash.com/photo-1645973441891-975bcf46ba48?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxMnx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60",
+    imgSrc:
+      "https://images.unsplash.com/photo-1502640403806-cf2ac7a5d37a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTN8fGVneXB0fGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
   },
 ];
 const SlideApp = () => {
-  const [current, setCurrent] = useState(0);
-  const length = slides.length;
-
-  if (!Array.isArray(slides) || length <= 0) return;
-
-  function nextSlide() {
-    setCurrent(current === length - 1 ? 0 : current + 1);
-  }
-  function prevSlide() {
-    setCurrent(current === 0 ? length - 1 : current - 1);
-  }
+  const [current, setCurrent] = React.useState(0);
   console.log(current);
+  const handleNext = () => {
+    let index = current >= imageData.length - 1 ? 0 : current + 1;
 
+    setCurrent(index);
+  };
+  const handlePrev = () => {
+    let index = current <= 0 ? imageData.length - 1 : current - 1;
+
+    setCurrent(index);
+  };
   return (
-    <>
-      <button id="prev" onClick={prevSlide}>
-        Prev
-      </button>
-      <button id="next" onClick={nextSlide}>
-        Next
-      </button>
-      <div className="slides">
-        {slides.map((slide, index) => (
-          <div className={index === current ? "slide active" : "slide"}>
-            {index === current && (
-              <img key={slide._id} src={slide.image} alt="hello" />
-            )}
-          </div>
-        ))}
-      </div>
-    </>
+    <div>
+      <button onClick={handlePrev}>Prev</button>
+      <button onClick={handleNext}>Next</button>
+      {imageData.map((img, index) => (
+        <div>
+          {current === index && (
+            <img src={img.imgSrc} key={img._id} alt="hello" />
+          )}
+        </div>
+      ))}
+    </div>
   );
 };
 
