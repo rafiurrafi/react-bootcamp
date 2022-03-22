d367d0a1a0a54b65bc8a4103f666464fc371b2f4
Rafiur Rahman <rrfoundary@gmail.com>
2022-03-22 14:39:25 +0600

    crown - refactor home page to menu item component

--- src/crown/crownApp.jsx
+++ src/crown/crownApp.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import Homepage from "./components/homepage/homepage";
+import Homepage from "./pages/homepage/homepage";
 class CrownApp extends Component {
   state = {};
   render() {
