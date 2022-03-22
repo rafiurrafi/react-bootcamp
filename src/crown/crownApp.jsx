7e826a5435e497e96e125dbcf8317d7cdbc32e53
Rafiur Rahman <rrfoundary@gmail.com>
2022-03-22 13:17:46 +0600

    crown - added home page structure component

--- src/crown/crownApp.jsx
+++ src/crown/crownApp.jsx
@@ -1,8 +1,13 @@
 import React, { Component } from "react";
+import Homepage from "./components/homepage/homepage";
 class CrownApp extends Component {
   state = {};
   render() {
-    return <div className="crown-app">Crown app</div>;
+    return (
+      <div className="crown-app">
+        <Homepage />
+      </div>
+    );
   }
 }
 
