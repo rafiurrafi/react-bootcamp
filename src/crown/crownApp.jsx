9823af133f546ee282aa04fb6443138dcf27d52c
Rafiur Rahman <rrfoundary@gmail.com>
2022-03-24 16:23:07 +0600

    crown - added form input componentn

--- src/crown/crownApp.jsx
+++ src/crown/crownApp.jsx
@@ -3,14 +3,18 @@ import Homepage from "./pages/homepage/homepage";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import ShopPage from "./pages/shop/shop";
 import "./crownApp.scss";
+import Header from "./components/header/header";
+import SignInAndSignOut from "./pages/signInAndSignOutPage/signInAndSignOutPage";
 class CrownApp extends Component {
   state = {};
   render() {
     return (
       <BrowserRouter>
         <div className="crown-app">
+          <Header />
           <Switch>
             <Route path="/shop" component={ShopPage} />
+            <Route path="/signin" component={SignInAndSignOut} />
             <Route path="/" component={Homepage} />
           </Switch>
         </div>
---
+++ src/crown/pages/signInAndSignOutPage/signInAndSignOutPage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import SignIn from "../../components/signIn/signIn";
+import "./signInAndSignOutPage.scss";
+const SignInAndSignOut = () => {
+  return (
+    <div className="sign-in-and-out">
+      <SignIn />
+    </div>
+  );
+};
+
+export default SignInAndSignOut;
