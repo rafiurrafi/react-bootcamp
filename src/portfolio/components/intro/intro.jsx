233e7671c8d8c799ee144f445ced1d21faa85e4a
Rafiur Rahman <rrfoundary@gmail.com>
2022-04-13 18:09:38 +0600

    added right section of portfolio

--- src/portfolio/components/intro/intro.jsx
+++ src/portfolio/components/intro/intro.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import "./intro.scss";
 import men from "../../img/men.png";
+import * as fa from "react-icons/fa";
+import { init } from "ityped";
 const Intro = () => {
   return (
     <div className="intro" id="intro">
@@ -9,7 +11,14 @@ const Intro = () => {
           <img src={men} alt="Logo" />
         </div>
       </div>
-      <div className="right"></div>
+      <div className="right">
+        <h4>Hi There, I'm </h4>
+        <h1>Hanna Khan</h1>
+        <h5>Freelancer</h5>
+        <button>
+          <fa.FaArrowDown />
+        </button>
+      </div>
     </div>
   );
 };
