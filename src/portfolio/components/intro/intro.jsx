330ffb1afe394d45c02acdaa3b7d51c5a96e4134
Rafiur Rahman <rrfoundary@gmail.com>
2022-04-13 18:16:13 +0600

    added ityped in intro section

--- src/portfolio/components/intro/intro.jsx
+++ src/portfolio/components/intro/intro.jsx
@@ -1,9 +1,21 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import "./intro.scss";
 import men from "../../img/men.png";
 import * as fa from "react-icons/fa";
 import { init } from "ityped";
 const Intro = () => {
+  // componentDidMount(){
+  //   const myElement = document.querySelector('#myElement')
+  //   init(myElement, { showCursor: false, strings: ['Use with React.js!', 'Yeah!' ] })
+  // }
+  const textRef = useRef();
+
+  useEffect(() => {
+    init(textRef.current, {
+      showCursor: false,
+      strings: ["React ", "Javascript", "CSS"],
+    });
+  }, []);
   return (
     <div className="intro" id="intro">
       <div className="left">
@@ -14,7 +26,9 @@ const Intro = () => {
       <div className="right">
         <h4>Hi There, I'm </h4>
         <h1>Hanna Khan</h1>
-        <h5>Freelancer</h5>
+        <h5>
+          <span ref={textRef}></span> Developer
+        </h5>
         <button>
           <fa.FaArrowDown />
         </button>
