cdd5878bb6b69dbaacffe0ce75c131f537cfdfdb
Rafiur Rahman <rrfoundary@gmail.com>
2022-05-05 11:32:55 -0700

    start working with lagacy version

--- src/crown/App.js
+++ src/crown/App.js
@@ -1,32 +1,35 @@
-import { useContext } from "react";
-import { Routes, Route, Navigate } from "react-router-dom";
+import Directory from './components/directory/directory.component';
 
-import { UserContext } from "./contexts/user.context";
+const App = () => {
+  const categories = [
+    {
+      id: 1,
+      title: 'hats',
+      imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
+    },
+    {
+      id: 2,
+      title: 'jackets',
+      imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
+    },
+    {
+      id: 3,
+      title: 'sneakers',
+      imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
+    },
+    {
+      id: 4,
+      title: 'womens',
+      imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
+    },
+    {
+      id: 5,
+      title: 'mens',
+      imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
+    },
+  ];
 
-import Home from "./routes/home/home.component";
-import Navigation from "./routes/navigation/navigation.component";
-import Authentication from "./routes/authentication/authentication.component";
-
-const Shop = () => {
-  return <h1>I am the shop page</h1>;
+  return <Directory categories={categories} />;
 };
 
-const CrownApp = () => {
-  const { currentUser } = useContext(UserContext);
-  return (
-    <Routes>
-      <Route path="/" element={<Navigation />}>
-        <Route index element={<Home />} />
-        <Route path="shop" element={<Shop />} />
-        <Route
-          path="auth"
-          element={
-            currentUser ? <Navigate to="/" replace /> : <Authentication />
-          }
-        />
-      </Route>
-    </Routes>
-  );
-};
-
-export default CrownApp;
+export default App;
