d93520a4299f59b8dd959d5847a14c41793da0e0
Rafiur Rahman <rrfoundary@gmail.com>
2022-06-06 11:02:15 +0600

    added context to shop page

--- src/crown/components/directory/directory.component.jsx
+++ src/crown/components/directory/directory.component.jsx
@@ -1,14 +1,14 @@
-import DirectoryItem from '../directory-item/directory-item.component';
+import CategoryItem from '../category-item/category-item.component';
 
-import { DirectoryContainer } from './directory.styles';
+import './directory.styles.scss';
 
 const Directory = ({ categories }) => {
   return (
-    <DirectoryContainer>
+    <div className='directory-container'>
       {categories.map((category) => (
-        <DirectoryItem key={category.id} category={category} />
+        <CategoryItem key={category.id} category={category} />
       ))}
-    </DirectoryContainer>
+    </div>
   );
 };
 
