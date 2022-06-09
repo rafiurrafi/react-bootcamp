bb44a57484dde5f313b3a57bbe30822ffe2cb3de
Rafiur Rahman <rrfoundary@gmail.com>
2022-06-09 09:42:31 +0600

    added router in crown clothing

--- src/crown/App.js
+++ src/crown/App.js
@@ -1,21 +1,11 @@
 import { Routes, Route } from 'react-router-dom';
 
-import Home from './routes/home/home.component';
-import Navigation from './routes/navigation/navigation.component';
-import Authentication from './routes/authentication/authentication.component';
-
-const Shop = () => {
-  return <h1>I am the shop page</h1>;
-};
+import Home from './routes/home/home.componentt';
 
 const App = () => {
   return (
     <Routes>
-      <Route path='/' element={<Navigation />}>
-        <Route index element={<Home />} />
-        <Route path='shop' element={<Shop />} />
-        <Route path='auth' element={<Authentication />} />
-      </Route>
+      <Route path='/' index element={<Home />} />
     </Routes>
   );
 };
--- src/index.js
+++ src/index.js
@@ -1,20 +1,15 @@
-import React from "react";
-import { render } from "react-dom";
-import { BrowserRouter } from "react-router-dom";
+import React from 'react';
+import { render } from 'react-dom';
+import './index.scss';
+import App from './App';
+import { BrowserRouter } from 'react-router-dom';
 
-import App from "./crown/App";
-import { UserProvider } from "./crown/contexts/user.context";
-
-import "./crown/index.scss";
-
-const rootElement = document.getElementById("root");
+const rootElement = document.getElementById('root');
 
 render(
   <React.StrictMode>
     <BrowserRouter>
-      <UserProvider>
-        <App />
-      </UserProvider>
+      <App />
     </BrowserRouter>
   </React.StrictMode>,
   rootElement
