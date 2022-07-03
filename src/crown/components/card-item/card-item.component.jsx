ac5884040087a54ee046b5f1827f82fc90cd326e
Rafiur Rahman <rrfoundary@gmail.com>
2022-07-03 11:37:15 +0600

    added card item add functionality

---
+++ src/crown/components/card-item/card-item.component.jsx
@@ -0,0 +1,9 @@
+const CardItem = ({ cardItem }) => {
+  return (
+    <div>
+      <h4>{cardItem.name}</h4>
+    </div>
+  );
+};
+
+export default CardItem;
--- src/crown/context/card.context.js
+++ src/crown/context/card.context.js
@@ -1,13 +1,26 @@
 import { createContext, useState } from "react";
-
+const addCardItem = (cardItems, itemToAdd) => {
+  const existingItem = cardItems.find((item) => item.id === itemToAdd.id);
+  if (existingItem)
+    return cardItems.map((item) =>
+      item.id === itemToAdd.id ? { ...item, quantity: item.quantity + 1 } : item
+    );
+  return [...cardItems, { ...itemToAdd, quantity: 1 }];
+};
 export const CardContext = createContext({
   cardOpen: false,
   setCardOpen: () => {},
+  cardItems: [],
+  addItemToCard: () => {},
 });
 
 const CardProvider = ({ children }) => {
   const [cardOpen, setCardOpen] = useState(false);
+  const [cardItems, setCardItems] = useState([]);
   const value = { cardOpen, setCardOpen };
+  const addItemToCard = (itemToAdd) => {
+    setCardItems(addCardItem(itemToAdd));
+  };
   return <CardContext.Provider value={value}>{children}</CardContext.Provider>;
 };
 
