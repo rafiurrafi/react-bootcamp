e646397ddf501eb17b860d1fc17a5766957c2308
Rafiur Rahman <rrfoundary@gmail.com>
2022-07-04 00:22:13 +0600

    refactor to user reducer

--- src/crown/context/user.context.jsx
+++ src/crown/context/user.context.jsx
@@ -4,8 +4,23 @@ export const UserContext = useContext({
   currentUser: null,
   setCurrentUser: () => null,
 });
+
+const userReducer = (state, action) => {
+  const { type, payload } = action;
+  switch (type) {
+    case "SET_CURRENT_USER":
+      return { user: payload };
+
+    default:
+      throw new Error(`unhandled action type ${type}`);
+  }
+};
+const INITIAL_VALUE = { currentUser: null };
+
 const UserProvider = ({ children }) => {
-  const [currentUser, setCurrentUser] = useState(null);
+  // const [currentUser, setCurrentUser] = useState(null);
+  const [{ currentUser }, dispatch] = userReducer(userReducer, INITIAL_VALUE);
+  const setCurrentUser = (user) => dispatch(user);
   const value = { currentUser, setCurrentUser };
   return <UserContext.Provider value={value}>{children}</UserContext.Provider>;
 };
