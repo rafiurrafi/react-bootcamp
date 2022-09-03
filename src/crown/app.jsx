a35a974f93a45e40a37e7b3f43f2f63d1e09f4a9
Rafiur Rahman <rrfoundary@gmail.com>
2022-09-03 20:11:06 +0600

    learn split layout using layout component

--- src/crown/app.jsx
+++ src/crown/app.jsx
@@ -1,5 +1,15 @@
+import SplitScreen from "./split-screen";
+const Left = () => <h1>Hello</h1>;
+const Right = () => <h1>Right</h1>;
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <SplitScreen leftWeight={1} rightWeight={3}>
+        <Left />
+        <Right />
+      </SplitScreen>
+    </div>
+  );
 };
 
 export default App;
