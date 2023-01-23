edd508defa79353b7809ed86c32c901d7533ff74
Rafiur Rahman <rrfoundary@gmail.com>
2023-01-23 15:09:14 +0600

    added scroll infite event

--- src/practise/practiseApp.jsx
+++ src/practise/practiseApp.jsx
@@ -1,8 +1,34 @@
-import BlogInformation from "./blog-information";
+import { useEffect, useState } from "react";
+
 const PractiseApp = () => {
+  const [posts, setPosts] = useState([]);
+  const [page, setPage] = useState(1);
+
+  function func() {
+    if (
+      window.innerHeight + document.documentElement.scrollTop + 1 >=
+      document.documentElement.scrollHeight
+    ) {
+      setPage((page) => page + 1);
+    }
+  }
+
+  useEffect(() => {
+    fetch(`https://jsonplaceholder.typicode.com/posts/?_limit=9&_page=${page}`)
+      .then((res) => res.json())
+      .then((data) => setPosts((prev) => [...prev, ...data]));
+  }, [page]);
+  useEffect(() => {
+    window.addEventListener("scroll", func);
+  }, []);
   return (
     <div>
-      <BlogInformation author="Adhithi" article="New article" />
+      {posts.map(({ id, title, body }) => (
+        <div style={{ padding: "3rem 0" }}>
+          <h1>{title}</h1>
+          <p>{body}</p>
+        </div>
+      ))}
     </div>
   );
 };
